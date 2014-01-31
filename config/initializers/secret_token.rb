f4d9e5bf672f973c8cd400e11abbe151b362b9b0
SHIMADA Koji <koji.shimada@enishi-tech.com>
2014-02-01 00:08:37 +0900

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HonNoSukoshi::Application.config.secret_key_base = '1645d3b2e4c86a405f1266ac7b4c3bbd22cacb70c1bc739e2d0761a7c386736160062ebddded9ea2fc4b3f3f44afa155998554b9f0bd0cc2d626afd0e12f193f'
