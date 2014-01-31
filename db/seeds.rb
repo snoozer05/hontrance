de8a04e5f07600e927d2d0655ff0adb5c2aa73e4
SHIMADA Koji <koji.shimada@enishi-tech.com>
2014-02-01 00:22:14 +0900

    Add crawler

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'nokogiri'
+require 'open-uri'
+
+last_quote = Quote.last
+start_id = last_quote ? (last_quote.booklog_id + 1) : 1
+end_id = start_id + 1000
+start_id.upto(end_id).each do |booklog_id|
+  begin
+    doc = Nokogiri::HTML(open("http://booklog.jp/quote/#{booklog_id}"))
+    body = doc.xpath("//blockquote").text
+    book_title = doc.css(".citeArea").children[1].children[1].text
+    booklog_url = doc.css(".quoteBook").attr("href").value
+    quote = Quote.create(body: body, book_title: book_title, booklog_url: booklog_url, booklog_id: booklog_id)
+    p quote
+  rescue => e
+    p e
+  end
+end
