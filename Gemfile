de9a7f5a1917d9fe43c2e5be0c54473cf3cfcd71
SHIMADA Koji <koji.shimada@enishi-tech.com>
2014-07-03 17:22:00 +0900

    Use rails 4.1.4

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 ruby '2.2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.0.2'
+gem 'rails', '4.1.4'
 
 # Use sqlite3 as the database for Active Record
 group :development do
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Hontrance::Application.config.session_store :cookie_store, key: '_hon-no-sukoshi_session'
+Rails.application.config.session_store :cookie_store, key: '_hontrance_session'
