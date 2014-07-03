73b9ef0aa4ad92b024d53a903d40357b9cf4b63a
SHIMADA Koji <koji.shimada@enishi-tech.com>
2014-07-03 23:18:42 +0900

    Remove unnecessary routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Hontrance::Application.routes.draw do
   root 'quotes#index'
-  resources :quotes, only: [:index]
   get 'about' => 'quotes#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
