b29a288582e94bac898764d68d5f5addf4236aeb
SHIMADA Koji <koji.shimada@enishi-tech.com>
2014-07-03 23:22:56 +0900

    Add minimum validations

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,6 @@
 class Quote < ActiveRecord::Base
+  validates :body, presence: true
+  validates :book_title, presence: true
+  validates :booklog_url, presence: true
+  validates :booklog_id, presence: true
 end
